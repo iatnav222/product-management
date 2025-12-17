31b8527ac3d9507520e43f780c76898a05a9e570
iatnav222 <tranvntai222@gmail.com>
2025-12-18 04:11:34 +0700

    Update header title in index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 </head>
 <body>
     <div class="header">
-        <h1>HỆ THỐNG QUẢN LÝ KHO (Render & Docker)</h1>
+        <h1>HỆ THỐNG QUẢN LÝ KHO a (Render & Docker)</h1>
         <p>Sinh viên thực hiện: Trần Văn Tài</p>
     </div>
 
